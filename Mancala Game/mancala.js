3c46cca85021aa9dae68060f7109ee32080945d1
Brandon Shaver <bnn678@users.noreply.github.com>
2018-09-04 15:44:43 -0500

    Added Removing Pebbles

--- Mancala Game/mancala.js
+++ Mancala Game/mancala.js
@@ -56,20 +56,23 @@ function initPebbles()
 
 function takeTurn(columnIndex)
 {
-  if (turn == 0 && columnIndex < 7)
+  if( board[columnIndex] > 0 )
   {
-    addPebbles(columnIndex, turn);
-    turn = 1;
-  }
-  else if (turn == 1 && columnIndex > 7)
-  {
-    addPebbles(columnIndex, turn);
-    turn = 0;
-  }
+    if (turn == 0 && columnIndex < 7)
+    {
+      addPebbles(columnIndex, turn);
+      turn = 1;
+    }
+    else if (turn == 1 && columnIndex > 7)
+    {
+      addPebbles(columnIndex, turn);
+      turn = 0;
+    }
 
-  displayTurn();
-  displayScore();
-  checkEnd();
+    displayTurn();
+    displayScore();
+    checkEnd();
+  }
 }
 
 function addPebbles(columnIndex)
@@ -78,9 +81,10 @@ function addPebbles(columnIndex)
   {
     addPebble( ((columnIndex+i+1)%14) );
     board[ ((columnIndex+i+1)%14) ] += 1;
+    removePebble(columnIndex);
   }
 
-  //board[columnIndex] = 0; // to be added once I figure out how to remove pebbles
+  board[columnIndex] = 0;
 }
 
 function addPebble(columnIndex)
@@ -93,14 +97,82 @@ function addPebble(columnIndex)
   columnElement.appendChild(pebble);
 }
 
+function removePebble(columnIndex)
+{
+  document.getElementById("column-"+columnIndex).firstChild.remove();
+}
+
 function checkEnd()
 {
+  var gameOver = false;
+
   var sum = board[0] + board[7];
 
-  if( sum == 48)
+  var playerZeroHasMoves = false;
+  var playerOneHasMoves = false;
+
+  for (var i = 1; i < 7; i++)
+  {
+    if (board[i] != 0)
+    {
+      playerZeroHasMoves = true;
+    }
+    if (board[i+7] != 0)
+    {
+      playerOneHasMoves = true;
+    }
+  }
+
+  if( sum == 48 || playerZeroHasMoves == false || playerOneHasMoves == false )
+  {
+    gameOver = true;
+  }
+
+  if (!playerOneHasMoves)
+  {
+    moveAllPebbles(0);
+  }
+  if (!playerZeroHasMoves)
+  {
+    moveAllPebbles(1);
+  }
+
+  if (gameOver)
   {
     displayMessage("Game is over!! Refresh the page to start the game again.");
   }
+}
+
+function moveAllPebbles(player)
+{
+  if (player == 0)
+  {
+    for (var i = 1; i < 7; i++)
+    {
+      for (var j = 0; j < board[i]; j++)
+      {
+        removePebble(i);
+        addPebble(7);
+        board[7] += 1;
+      }
+      board[i] = 0;
+    }
+  }
+  if (player == 1)
+  {
+    for (var i = 8; i < 14; i++)
+    {
+      for (var j = 0; j < board[i]; j++)
+      {
+        removePebble(i);
+        addPebble(0);
+        board[0] += 1;
+      }
+      board[i] = 0;
+    }
+  }
+
+  displayScore();
 }
 
 initPebbles();
